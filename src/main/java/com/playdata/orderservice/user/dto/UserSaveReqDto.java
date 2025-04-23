34d37adb68003467a1f14509003b87681016ea8c
hajoon <stravinskky@naver.com>
2025-04-23 14:24:18 +0900

    비밀번호 암호화 처리

--- src/main/java/com/playdata/orderservice/user/dto/UserSaveReqDto.java
+++ src/main/java/com/playdata/orderservice/user/dto/UserSaveReqDto.java
@@ -5,6 +5,7 @@ import com.playdata.orderservice.user.entity.User;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 // 데이터 단순 운반 용도로 사용하는 DTO
 // 프론트 단을 ㅗ전달할, 전달받은 데이터는 따로 DTO를 선언하는 것을 권장.
@@ -30,11 +31,11 @@ public class UserSaveReqDto {
     private Address Address;
 
     // dto가 자기가 가지고 있는 필드 정보를 토대로 User entity를 생성해서 리턴하는 메서드
-    public User toEntity(){
+    public User toEntity(PasswordEncoder encoder){
         return User.builder()
                 .name(this.Name)
                 .email(this.Email)
-                .password(this.Password)
+                .password(encoder.encode(this.Password))
                 .address((this.Address))
                 .build();
     }
