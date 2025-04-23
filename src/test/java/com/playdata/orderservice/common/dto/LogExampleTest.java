73ffadcdc9e9f2249cbb630b7c72e1a4060e2dfd
hajoon <stravinskky@naver.com>
2025-04-23 15:06:28 +0900

    로그 설정 및 레벨 파악하기

---
+++ src/test/java/com/playdata/orderservice/common/dto/LogExampleTest.java
@@ -0,0 +1,27 @@
+package com.playdata.orderservice.common.dto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LogExampleTest {
+
+    @Autowired
+    LogExample logExample;
+
+    @Test
+    @DisplayName("로그 레벨 확인")
+    void logTest() {
+        // given
+
+        // when
+        logExample.showLog();
+
+        // then
+
+    }
+}
