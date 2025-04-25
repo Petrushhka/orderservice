2c31c9e0d0e4d8a3ab7ea34f9553bd2c18f6de76
hajoon <stravinskky@naver.com>
2025-04-25 14:49:03 +0900

    Redis Config 설정, 로그인 시 데이터 삽입(refresh token)

--- src/main/java/com/playdata/orderservice/user/controller/UserController.java
+++ src/main/java/com/playdata/orderservice/user/controller/UserController.java
@@ -10,6 +10,7 @@ import com.playdata.orderservice.user.entity.User;
 import com.playdata.orderservice.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -20,6 +21,7 @@ import org.springframework.data.domain.Pageable;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
 @RestController
@@ -29,6 +31,7 @@ public class UserController {
     // 빈 등록된 서비스 객체를 자동으로 주입받자!
     private final UserService userService;
     private final JwtTokenProvider jwtTokenProvider;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     /*
      프론트 단에서 회원 가입 요청 보낼때 함께 보내는 데이터 (JSON) -> dto로 받자!!!!!
@@ -76,6 +79,7 @@ public class UserController {
 
         //refreshToken을 DB에 저장하자.
 //        userService.saveRefreshToken(user.getEmail(),refreshToken);
+          redisTemplate.opsForValue().set("user:refresh:"+user.getId(), refreshToken, 2, TimeUnit.MINUTES);
 
 
         //Map을 이용해서 사용자의 id와 token을 포장하자.
